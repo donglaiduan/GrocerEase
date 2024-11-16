361a8ae962d59bf5bbf9ab2f0186b6d981077e81
Jonathan Yu <jcyu5@wisc.edu>
2024-11-16 13:37:51 -0600

    Initial commit

---
+++ app/src/main/java/com/cs407/grocerease/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.cs407.grocerease.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is Notification Fragment"
+    }
+    val text: LiveData<String> = _text
+}
