2d6787b30eaea2375b73d7945bcaa4d287a7f68b
junny <jchiang1218@gmail.com>
2024-11-18 14:40:32 -0600

    App design for List (no implementation for switching Fragments yet implemented, skeleton made)

--- app/src/main/java/com/cs407/grocerease/ui/recommendations/RecommendationsViewModel.kt
+++ app/src/main/java/com/cs407/grocerease/ui/recommendations/RecommendationsViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class RecommendationsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is Recommendations Fragment"
+        value = "Recipes List"
     }
     val text: LiveData<String> = _text
 }
