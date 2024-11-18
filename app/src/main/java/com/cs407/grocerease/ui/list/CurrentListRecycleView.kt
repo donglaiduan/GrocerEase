b784b5739872fa8d99250c8754e79ccdb573c8ca
Abhi Padala <apadala@wisc.edu>
2024-11-18 01:50:09 -0600

    Created layout for fragment list. Can add items and list - beginning phase - still buggy

---
+++ app/src/main/java/com/cs407/grocerease/ui/list/CurrentListRecycleView.kt
@@ -0,0 +1,36 @@
+package com.cs407.grocerease.ui.list
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.cs407.grocerease.R
+
+class CurrentListRecycleView (
+    private val groceryItems: MutableList<GroceryItem>,
+    private val onDeleteClick: (Int) -> Unit
+) : RecyclerView.Adapter<CurrentListRecycleView.GroceryItemsView>() {
+
+    inner class GroceryItemsView(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val itemName: TextView = itemView.findViewById(R.id.ItemName)
+        val deleteButton: ImageView = itemView.findViewById(R.id.DeleteItemButton)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GroceryItemsView {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_grocery_list, parent, false)
+        return GroceryItemsView(view)
+    }
+
+    override fun onBindViewHolder(holder: GroceryItemsView, position: Int) {
+        val item = groceryItems[position]
+        holder.itemName.text = item.name
+        holder.deleteButton.setOnClickListener {
+            onDeleteClick(position)
+        }
+    }
+
+    override fun getItemCount(): Int = groceryItems.size
+
+}
---
+++ app/src/main/java/com/cs407/grocerease/ui/list/GroceryItem.kt
@@ -0,0 +1,6 @@
+package com.cs407.grocerease.ui.list
+
+data class GroceryItem(
+    var name: String,
+    var amount: Int
+)
