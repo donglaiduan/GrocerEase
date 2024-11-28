4bfa86ef2cee1815beb4c25b73364bdab1471a9b
Donglai Duan <donglaiduann@gmail.com>
2024-11-27 18:51:28 -0600

    add skeleton of add blog dialog

--- app/src/main/java/com/cs407/grocerease/ui/home/HomeFragment.kt
+++ app/src/main/java/com/cs407/grocerease/ui/home/HomeFragment.kt
@@ -1,11 +1,14 @@
 package com.cs407.grocerease.ui.home
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
@@ -39,6 +42,7 @@ class HomeFragment : Fragment() {
             textView.text = it
         }
         val addBlogButton = root.findViewById<Button>(R.id.addBlogButton)
+        val addBlog = root.findViewById<ImageView>(R.id.addBlog)
 
         addBlogButton.setOnClickListener {
             Log.d("Blog Button", "Add Blog Button Clicked!")
@@ -64,11 +68,47 @@ class HomeFragment : Fragment() {
                 }
         }
 
+        addBlog.setOnClickListener {
+            addBlogPopup()
+        }
+
         return root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun addBlogPopup() {
+        val dialogView = LayoutInflater.from(requireContext())
+            .inflate(R.layout.adding_blog_dialog, null)
+        val titleEditText = dialogView.findViewById<EditText>(R.id.title_edit_text)
+        val bodyEditText = dialogView.findViewById<EditText>(R.id.body_edit_text)
+        val imageView = dialogView.findViewById<ImageView>(R.id.image_view)
+        val cancelButton = dialogView.findViewById<Button>(R.id.cancel_button)
+        val uploadButton = dialogView.findViewById<Button>(R.id.upload_button)
+
+        val builder = AlertDialog.Builder(requireContext())
+            .setView(dialogView)
+            .setCancelable(true)
+
+        val dialog = builder.create()
+        dialog.show()
+
+        cancelButton.setOnClickListener {
+            dialog.dismiss()
+        }
+
+        uploadButton.setOnClickListener {
+            val title = titleEditText.text.toString()
+            val body = bodyEditText.text.toString()
+
+            // Here, you can add your logic to save the blog post to Firestore
+
+
+            dialog.dismiss()
+        }
+
     }
 }
